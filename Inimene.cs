8f46faad0025829f3b766b0a44a88ac3ee0c5d40
Opilane TTHK <opilane@tthk.ee>
2022-02-28 13:25:17 +0200

    Add project files.

---
+++ Inimene.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_TARpv21
+{
+    public class Inimene
+    {
+        string eesnimi;
+        int vanus;
+        string staatus;
+        string perekonanimi;
+        int isikukood;
+        string pool; 
+
+
+
+        public Inimene() { }//Конструктор
+        public Inimene(string Eesnimi, int Vanus)
+        {
+            eesnimi = Eesnimi;
+            vanus = Vanus; 
+        }
+        public string Eesnimi
+        {
+            set 
+            {
+                if (eesnimi == null) eesnimi = value; 
+            }
+            get { return eesnimi; }
+        }//св-ва
+        public string Perekonanimi
+        {
+            set
+            {
+                perekonanimi = value;
+            }
+            get { return eesnimi; }
+        }
+        public int Vanus
+        {
+            set { vanus = value;
+                if (vanus<7)
+                {
+                    staatus = "väike laps";
+                }
+                else if (vanus<17)
+                {
+                    staatus = "Kooli laps";
+                }
+                else if (vanus<65)
+                {
+                    staatus = "tööline inimene";
+                }
+                else
+                {
+                    staatus = "senior";
+                }
+            
+            }
+            get { return vanus;  } 
+        }
+        public int Isikukood
+        { 
+            set 
+            { 
+                isikukood = value ;
+                char[] arr = isikukood.ToString().ToCharArray();
+                if (arr[0] == 1 )
+                {
+                    pool = "1800. ... 1899. a sündinud mees";
+                }
+                else if (arr[0] == 2)
+                {
+                    pool = "kui 1800. ... 1899. a sündinud naine";
+                }
+                else if (arr [0]== 3)
+                {
+                    pool = "kui 1900. ... 1999. a sündinud mees";
+                }
+                else if (arr[0] == 4)
+                {
+                    pool = " kui 1900. ... 1999. a sündinud naine";
+                }
+                else if (arr[0] == 5)
+                {
+                    pool = "kui 1900. ... 1999. a sündinud mees";
+                }
+                else if (arr[0] == 6)
+                {
+                    pool = "kui 1900. ... 1999. a sündinud mees";
+                }
+                else if (arr[0] == 7)
+                {
+                    pool = "kui 1900. ... 1999. a sündinud mees";
+                }
+                else if (arr[0] == 8)
+                {
+                    pool = "kui 1900. ... 1999. a sündinud mees";
+                }
+
+            }
+        public string Staatus
+        {
+            set { staatus = value; }
+            get { return staatus;  }
+        }
+        public void Tervitame()//Метод
+        {
+            Console.WriteLine("Tere!");
+            Console.WriteLine("Minu nimi on {0}. Olen {1} aastat vana. Minu staatus on {2}", eesnimi, vanus, staatus);
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP_TARpv21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inimene[] inimesed = new Inimene[3];
+            for (int i = 0; i < 3; i++)
+            {
+                inimesed[i] = new Inimene();
+                Console.Write("Sisesta Nimi: ");
+                inimesed[i].Eesnimi = Console.ReadLine();
+                Console.Write("Sisesta Vanus: ");
+                inimesed[i].Vanus = int.Parse(Console.ReadLine());
+            }
+            foreach (Inimene inimene in inimesed)
+            {
+                inimene.Tervitame();
+            }
+
+            Inimene mees = new Inimene();
+            mees.Eesnimi = "Mati";
+            Console.WriteLine("Ma olen {0}. Kui vana ma olen?", mees.Eesnimi);
+            mees.Vanus = int.Parse(Console.ReadLine());
+            mees.Tervitame();
+
+            Inimene naine = new Inimene("Marina", 19);
+            naine.Tervitame();
+
+            Console.ReadKey();
+        }
+    }
+}
